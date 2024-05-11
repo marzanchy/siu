6a81fd3f5176005517383ee61c17735c0e650ad3
Marzan Chy <marzanchy40@gmail.com>
2024-05-12 01:29:42 +0600

    add two file

---
+++ Problemset-C/hello_world.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+//Write a C program to print "Hello World"
+int main() {
+
+  printf("Hello World!");
+
+  return 0;
+  // Solution:-1
+}
---
+++ Problemset-C/int_by_user.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+//Write a C Program to print an integer entered by the user.
+
+int main() {
+
+    int num;
+
+    printf("Enter integer Number: ");
+
+    scanf("%d", &num);
+
+    printf("Your integer number is: %d", num);
+
+    return 0;
+}
